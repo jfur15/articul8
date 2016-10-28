30a05c32d7c77b4bc7ce70a9f77bd443b2cdf040
Derek <derek.edwards@oit.edu>
2016-10-28 12:44:00 -0700

    Web Page Source Code Retriever. Look at README and try it out

---
+++ getsourcecode/GetSourceCode/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace GetSourceCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = URLTextBox.Text;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+            richTextBox1.Text = sr.ReadToEnd();
+            sr.Close();
+        }
+    }
+}
---
+++ getsourcecode/README.txt
@@ -0,0 +1,11 @@
+Example top result articles on the same subject found from a google search of the following keywords: FBI Hillary Clinton emails
+
+http://www.nytimes.com/2016/10/29/us/politics/fbi-hillary-clinton-email.html?_r=0
+
+http://abcnews.go.com/Politics/fbi-director-congressional-investigation-hillary-clinton-emails-back/story?id=43138105
+
+http://www.cnn.com/2016/10/28/politics/fbi-reviewing-new-emails-in-clinton-probe-director-tells-senate-judiciary-committee/
+
+http://www.latimes.com/nation/politics/trailguide/la-na-trailguide-updates-fbi-to-reopen-hillary-clinton-server-1477675545-htmlstory.html
+
+http://www.rollingstone.com/politics/news/fbi-investigating-new-emails-related-to-hillary-clinton-server-w447312
