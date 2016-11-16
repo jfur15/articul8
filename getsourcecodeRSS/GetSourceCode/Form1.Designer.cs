8b658b4dd1bf629bd5e9d0ac653673186eea6289
jfur15 <jfur15@yandex.com>
2016-11-15 19:44:09 -0800

    Added RSS version of C# program

---
+++ getsourcecodeRSS/GetSourceCode/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace GetSourceCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.URLTextBox = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Source Code";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // URLTextBox
+            // 
+            this.URLTextBox.Location = new System.Drawing.Point(36, 60);
+            this.URLTextBox.Name = "URLTextBox";
+            this.URLTextBox.Size = new System.Drawing.Size(117, 20);
+            this.URLTextBox.TabIndex = 1;
+            this.URLTextBox.TextChanged += new System.EventHandler(this.URLTextBox_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(177, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(750, 394);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 418);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.URLTextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Source Code";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox URLTextBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ getsourcecodeRSS/GetSourceCode/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace GetSourceCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = URLTextBox.Text;
+            XmlReader myXml = XmlReader.Create(url);
+            SyndicationFeed syn = SyndicationFeed.Load(myXml);
+            myXml.Close();
+            foreach (SyndicationItem item in syn.Items)
+            {
+                richTextBox1.AppendText(item.Title.Text);
+
+                richTextBox1.AppendText("\n\n");
+            }    
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void URLTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
