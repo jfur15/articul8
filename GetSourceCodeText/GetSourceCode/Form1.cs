80f7863b6073844f4bd8ee06cd0d32ecbe075341
Derek Edwards <Derek Edwards>
2016-11-21 17:45:59 -0800

    Updated Source Code Retriever: returns only title and text

---
+++ GetSourceCodeText/GetSourceCode/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace GetSourceCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = URLTextBox.Text;
+
+            var transcoder = new NReadability.NReadabilityWebTranscoder();
+            bool success;
+            string page = transcoder.Transcode(url, out success);
+
+            if (success)
+            {
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(page);
+
+                var title = doc.DocumentNode.SelectSingleNode("//title").InnerText;
+                //var imgUrl = doc.DocumentNode.SelectSingleNode("//meta[@property='og:image']").Attributes["content"].Value;
+                var mainText = doc.DocumentNode.SelectSingleNode("//div[@id='readInner']").InnerText;
+                richTextBox1.Text = title + "\n\n" + mainText;
+            }
+        }
+    }
+}
---
+++ GetSourceCodeText/ReadME.txt
@@ -0,0 +1,13 @@
+Extracts Text from source code pasted in from a URL in the following format:
+
+-Title
+
+-Body
+-
+-
+-
+
+Screenshot of results included
+Can also retrieve images
+
+Run the .exe shortcut
