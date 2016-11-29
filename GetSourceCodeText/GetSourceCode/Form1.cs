b5f7bb3741ab6f31ee3e2efcd036533aaadf55e5
Derek Edwards <Derek Edwards>
2016-11-29 14:23:17 -0800

    Initial Bullet Point Extractor Attempt / Added Comments

--- GetSourceCodeText/GetSourceCode/Form1.cs
+++ GetSourceCodeText/GetSourceCode/Form1.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// Team News Scraper:
+// Derek Edwards
+// Steven Reeves
+// Meagan Olsen
+// John Furlan
+//
+// News article scraping comparison program.
+//
+// Currently contains hardcoded examples of 2 news articles from the same source: 
+// The first was the original article, which was flattering to Presidential Candidate Bernie Sanders. 
+// The Second is an altered version of the same article that was released later 
+// that day and is highly critical of Bernie Sanders.
+//
+// Program goal is to extract the facts in the form of bullet points and eventually 
+// merge the two and get a clear, unbiased result.
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -31,8 +47,9 @@ namespace GetSourceCode
             string url2 = "http://www.nytimes.com/2016/03/15/us/politics/bernie-sanders-amendments.html";
             var transcoder = new NReadability.NReadabilityWebTranscoder();
             bool success;
+            // Transcode for url 1.
             string page1 = transcoder.Transcode(url1, out success);
-            //Transcode for url2
+            // Transcode for url 2.
             string page2 = transcoder.Transcode(url2, out success);
 
             if (success)
@@ -40,18 +57,54 @@ namespace GetSourceCode
                 HtmlAgilityPack.HtmlDocument doc1 = new HtmlAgilityPack.HtmlDocument();
                 doc1.LoadHtml(page1);
 
-                var title1 = doc1.DocumentNode.SelectSingleNode("//title").InnerText;
+                // Extract data from first article.
+                string title1 = doc1.DocumentNode.SelectSingleNode("//title").InnerText;
+                string mainText1 = doc1.DocumentNode.SelectSingleNode("//div[@id='readInner']").InnerText;
                 //var imgUrl = doc.DocumentNode.SelectSingleNode("//meta[@property='og:image']").Attributes["content"].Value;
-                var mainText1 = doc1.DocumentNode.SelectSingleNode("//div[@id='readInner']").InnerText;
-                richTextBox1.Text = title1 + "\n\n" + mainText1;
+
+
+                // Use space as delimiter to split string into individual words.
+                char[] delimiters = new char[] { ' ' };
+
+
+                // Split main body of text from article 1 into an array of strings.
+                string[] parts = mainText1.Split(delimiters);
+
+
+                // Format output to left display as individual sentences with line breaks in between.
+                foreach (string word in parts)
+                {
+                    richTextBox1.AppendText(title1 + "\n\n\n");
+                    richTextBox1.AppendText(word + " ");
+                    if ((word.Contains('.')) && (word != "Mr."))
+                    {
+                        if(word.Last() == '.') { richTextBox1.AppendText("\n\n"); }
+                    }
+                }
 
                 HtmlAgilityPack.HtmlDocument doc2 = new HtmlAgilityPack.HtmlDocument();
                 doc2.LoadHtml(page2);
 
-                var title2 = doc2.DocumentNode.SelectSingleNode("//title").InnerText;
+                // Extract data from second article.
+                string title2 = doc2.DocumentNode.SelectSingleNode("//title").InnerText;
+                string mainText2 = doc2.DocumentNode.SelectSingleNode("//div[@id='readInner']").InnerText;
                 //var imgUrl = doc.DocumentNode.SelectSingleNode("//meta[@property='og:image']").Attributes["content"].Value;
-                var mainText2 = doc2.DocumentNode.SelectSingleNode("//div[@id='readInner']").InnerText;
-                richTextBox2.Text = title2 + "\n\n" + mainText2;
+                
+
+                // Split main body of text from article 2 into an array of strings.
+                string[] parts2 = mainText2.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+
+                // Format output to right display as individual sentences with line breaks in between.
+                foreach (string word in parts2)
+                {
+                    richTextBox2.AppendText(title2 + "\n\n\n");
+                    richTextBox2.AppendText(word + " ");
+                    if ((word.Contains('.')) && (word != "Mr."))
+                    {
+                        if (word.Last() == '.') { richTextBox2.AppendText("\n\n"); }
+                    }
+                }
             }
         }
     }
---
+++ GetSourceCodeText/GetSourceCode/ReadMe.txt
@@ -0,0 +1,18 @@
+Team News Scraper:
+Derek Edwards
+Steven Reeves
+Meagan Olsen
+John Furlan
+
+News article scraping comparison program.
+
+Currently contains hardcoded examples of 2 news articles from the same source: 
+The first was the original article, which was flattering to Presidential Candidate Bernie Sanders. 
+The Second is an altered version of the same article that was released later 
+that day and is highly critical of Bernie Sanders.
+
+Program goal is to extract the facts in the form of bullet points and eventually 
+merge the two and get a clear, unbiased result.
+
+NOTE:
+Program is skipping the first few paragraphs in article 2.
