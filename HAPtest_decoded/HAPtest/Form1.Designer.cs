7b5225766071348979e4c2d9fe3933e0af60086f
Derek Edwards <derek.edwards@oit.edu>
2017-02-04 12:12:50 -0800

    refined algorithm, added improved iteration

--- HAPtest_decoded/HAPtest/Form1.Designer.cs
+++ HAPtest_decoded/HAPtest/Form1.Designer.cs
@@ -32,15 +32,16 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.Articles = new System.Windows.Forms.GroupBox();
+            this.Articles.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(8, 8);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 36);
+            this.button1.Size = new System.Drawing.Size(176, 34);
             this.button1.TabIndex = 0;
             this.button1.Text = "Make Magic";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,58 +49,55 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 54);
+            this.textBox1.Location = new System.Drawing.Point(5, 18);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(139, 26);
+            this.textBox1.Size = new System.Drawing.Size(161, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 86);
+            this.textBox2.Location = new System.Drawing.Point(5, 42);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(139, 26);
+            this.textBox2.Size = new System.Drawing.Size(161, 20);
             this.textBox2.TabIndex = 2;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(157, 12);
+            this.richTextBox1.Location = new System.Drawing.Point(188, 8);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(288, 307);
-            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Size = new System.Drawing.Size(693, 508);
+            this.richTextBox1.TabIndex = 5;
             this.richTextBox1.Text = "";
             // 
-            // richTextBox2
+            // Articles
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(468, 13);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(288, 307);
-            this.richTextBox2.TabIndex = 4;
-            this.richTextBox2.Text = "";
-            // 
-            // richTextBox3
-            // 
-            this.richTextBox3.Location = new System.Drawing.Point(302, 329);
-            this.richTextBox3.Name = "richTextBox3";
-            this.richTextBox3.Size = new System.Drawing.Size(288, 307);
-            this.richTextBox3.TabIndex = 5;
-            this.richTextBox3.Text = "";
+            this.Articles.Controls.Add(this.textBox1);
+            this.Articles.Controls.Add(this.textBox2);
+            this.Articles.Location = new System.Drawing.Point(12, 47);
+            this.Articles.Name = "Articles";
+            this.Articles.Size = new System.Drawing.Size(171, 100);
+            this.Articles.TabIndex = 6;
+            this.Articles.TabStop = false;
+            this.Articles.Text = "Articles";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(802, 648);
-            this.Controls.Add(this.richTextBox3);
-            this.Controls.Add(this.richTextBox2);
+            this.ClientSize = new System.Drawing.Size(892, 531);
+            this.Controls.Add(this.Articles);
             this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Super Program Alpha Team 6";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Articles.ResumeLayout(false);
+            this.Articles.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -109,8 +107,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
-        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.GroupBox Articles;
     }
 }
 
--- HAPtest_decoded/HAPtest/Form1.cs
+++ HAPtest_decoded/HAPtest/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Web;
 using HtmlAgilityPack;
+using System.Net;
 
 namespace HAPtest
 {
@@ -17,54 +18,59 @@ namespace HAPtest
         public Form1()
         {
             InitializeComponent();
-           
-        }
 
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-        private void URLGet(TextBox tb, RichTextBox rtb)
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<List<string>> allArticles = new List<List<string>>();
+            List<string> TextBoxUrlList = new List<string>();
+
+            foreach (TextBox T in Articles.Controls)
+            {
+                if (!string.IsNullOrEmpty(T.Text))
+                {
+                    TextBoxUrlList.Add(T.Text);
+                    AddArticle(allArticles);
+                }
+            }
+            if (TextBoxUrlList.Count() < 2)
+            {
+                MessageBox.Show("Invalid Input: Please enter URL into at least 2 fields");
+            }
+
+            for (int i = 0; i < TextBoxUrlList.Count; i++)
+            {
+                URLGet(TextBoxUrlList[i], allArticles[i]);
+            }
+
+            richTextBox1.AppendText("Hello!");
+        }
+
+        private void AddArticle(List<List<string>> listString)
+        {
+            List<string> UrlContent = new List<string>();
+            listString.Add(UrlContent);
+        }
+
+        private void URLGet(string tb, List<string> stringList)
         {
             HtmlWeb webPage = new HtmlWeb();
             webPage.UseCookies = true;
             //Try webPage.load(tb.text)
-            HtmlAgilityPack.HtmlDocument getHtmlWeb = webPage.Load(tb.Text);
+            HtmlAgilityPack.HtmlDocument getHtmlWeb = webPage.Load(tb);
             //HtmlAgilityPack.HtmlDocument getHtmlWeb = webPage.Load("https://web.archive.org/web/20160314164825/http://www.nytimes.com/2016/03/15/us/politics/bernie-sanders-amendments.html?partner=rss&emc=rss");
             List<HtmlNode> myNodes = new List<HtmlNode>();
             foreach (HtmlNode node in getHtmlWeb.DocumentNode.SelectNodes("//p"))
             {
-                rtb.Text += System.Net.WebUtility.HtmlDecode(node.InnerText);
-
-                rtb.AppendText("\n\n\n");
-
-                myNodes.Add(node);
+                stringList.Add(System.Net.WebUtility.HtmlDecode(node.InnerText));
             }
         }
-        private void button1_Click(object sender, EventArgs e)
-        {
-            List<TextBox> TextBoxList = new List<TextBox>();
-            TextBoxList.Add(textBox1);
-            TextBoxList.Add(textBox2);
-
-            //Check for 2 boxes of input
-            foreach (var tb in TextBoxList)
-            {
-                if (string.IsNullOrEmpty(tb.Text) || string.IsNullOrWhiteSpace(tb.Text))
-                {
-                    MessageBox.Show("Invalid Input: Please enter into all required fields");
-                }
-
-            }
-
-
-            URLGet(textBox1, richTextBox1);
-            URLGet(textBox2, richTextBox2);
-
-            //g.URLSplit(textBox1.Text, richTextBox1);
-        }
-
     }
 }
 
