8f69f01a187a18290dba0b082679a54db6043780
jfur15 <jfur15@yandex.com>
2017-02-28 18:12:31 -0800

    Implemented Regex sentence splitting method

--- HAPtest_decoded/HAPtest/Input/Paragraph.cs
+++ HAPtest_decoded/HAPtest/Input/Paragraph.cs
@@ -23,34 +23,9 @@ namespace HAPtest
             // Split paragraph into array of strings
 
             List<string> arraySentences = new List<string>();
-            string tempString = "";
-            int wordLen = 0; //wordLen represents how long a word has to be before we can split a sentence.
 
-            char[] sentenceEnders = { '.', '?', '!' };
-
-            //Iterate through every single character of aParagraph
-            for (int i = 0; i < aParagraph.Length; i++)
-            {
-                //Add each character to a temporary string and wordLen
-                tempString = tempString + aParagraph[i];
-                wordLen++;
-
-                //If a space appears, we need to reset wordLen.
-                if (char.IsWhiteSpace(aParagraph[i]))
-                {
-                    wordLen = 0;
-                }
-
-                //If there are at least 4 characters before a sentence ender, we split the paragraph
-                if (sentenceEnders.Contains(aParagraph[i]) && wordLen >= 4)
-                {
-                    arraySentences.Add(tempString);//Add to the main list of sentences
-                    tempString = "";        //Reset the temporary string
-                    wordLen = 0;            //reset the word length
-                }
-            }
-
-            foreach (string s in arraySentences)
+            Regex r = new Regex("(?<=[.?!;])\\s+(?=\\p{Lu})");
+            foreach (string s in r.Split(text))
             {
                 sentences.Add( new Sentence(s.Trim()));
             }
